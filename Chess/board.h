8da61a8e241c6cd0a03d1d69ba57beba46be23b6
arkelley77 <42101301+arkelley77@users.noreply.github.com>
2022-06-08 10:21:06 -0500

    Board class framing

--- Chess/board.h
+++ Chess/board.h
@@ -4,7 +4,34 @@
 // defines a complete board representation comprised of
 // both a BitBoard and a Mailbox
 
+#include <string>
+#include <sstream>
+
 #include "bitboard.h"
 #include "mailbox.h"
+
+class Board {
+public:
+  constexpr inline Board() noexcept : bit(), box() {}
+  constexpr inline Board(board& to_copy) noexcept 
+    : bit(to_copy.bit), box(to_copy.box) {}
+  
+  constexpr inline void setUp() noexcept;
+  void setUp(char* fen);
+  void setUp(std::string& fen);
+  
+  void loadPGN(char* filename);
+  void loadPGN(std::string& filename);
+  
+  std::string& toString() const noexcept;
+  
+  friend std::ostream& operator<<(std::ostream& os, Board& board) {
+    return os << board.box;
+  }
+  
+private:
+  BitBoard bit;
+  MailBox box;
+}
 
 #endif
