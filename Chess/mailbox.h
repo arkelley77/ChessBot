90bc9a7a258dd81661b78e7e3ac498ecd65f0ab5
arkelley77 <42101301+arkelley77@users.noreply.github.com>
2022-06-08 08:40:51 -0500

    Update mailbox.h

--- Chess/mailbox.h
+++ Chess/mailbox.h
@@ -1,1 +1,13 @@
+#ifndef MAILBOX_H
+#define MAILBOX_H
 
+// defines the mailbox representation of the board, which is
+// a different way of saying this is a wrapper class around
+// an array of 64 Piece::Names
+//
+// this kind of representation makes it easier to look up
+// a piece by square but makes most other operations inefficient
+
+#include "piece.h"
+
+#endif
