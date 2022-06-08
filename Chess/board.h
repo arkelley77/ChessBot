bbce3812ca369a4b526e9f691ad9f6aecc7ec33d
arkelley77 <42101301+arkelley77@users.noreply.github.com>
2022-06-08 08:59:19 -0500

    Create board.h

---
+++ Chess/board.h
@@ -0,0 +1,10 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+// defines a complete board representation comprised of
+// both a BitBoard and a Mailbox
+
+#include "bitboard.h"
+#include "mailbox.h"
+
+#endif
